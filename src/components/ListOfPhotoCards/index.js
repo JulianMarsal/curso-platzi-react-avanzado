39157d0a9c1a9c1ef562367d9c79d98942657228
Tu nombre <tu@ejemplo.cojulian_1997_01@hotmail.com>
2022-02-05 20:13:02 -0300

    Integration with react-apollo and graphql for fetching of data

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { PhotoCard } from "../PhotoCard";
+import { useQuery, gql } from "@apollo/client";
+
+const whitPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
 
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(whitPhotos);
+
+  if (error) {
+    return <h2>Internal Server Error</h2>;
+  }
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <ul>
-      {[1, 2, 3, 4, 5].map((id) => (
-        <PhotoCard key={id} id={id} />
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
       ))}
     </ul>
   );
