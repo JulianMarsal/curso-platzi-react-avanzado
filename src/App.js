3a38fa7da52f442dd336b8d31c827749be394df0
Tu nombre <tu@ejemplo.cojulian_1997_01@hotmail.com>
2022-02-12 14:33:43 -0300

    Implement protected routes

--- src/App.js
+++ src/App.js
@@ -6,27 +6,35 @@ import { Home } from "./pages/Home";
 import { NavBar } from "./components/NavBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Detail } from "./pages/Detail";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
+import { Favs } from "./pages/Favs";
+import { Register } from "./pages/Register";
+
 const urlParams = new window.URLSearchParams(window.location.search);
 const detailId = urlParams.get("detail");
 export const App = () => {
-  const { loading, error, data = {} } = useGetSinglePhoto(detailId);
-
-  {
-    //<PhotoCard key={data} {...data.photo} />
-  }
+  const isAuth = false;
   return (
     <div>
       <BrowserRouter>
         <GlobalStyle />
         <Logo />
         <Routes>
-          <Route
-            path="/detail/:detailId"
-            element={<Detail detailId={detailId} data={data} />}
-          ></Route>
-
-          <Route path="/pet/:id" element={<Home />}></Route>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/detail/:detailId" element={<Detail />} />
+          <Route path="/pet/:id" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          {isAuth ? (
+            <>
+              <Route path="/favs" element={<Favs />} />
+              <Route path="/user" element={<User />} />
+            </>
+          ) : (
+            <>
+              <Route path="/favs" element={<Favs />} />
+              <Route path="/user" element={<NotRegisteredUser />} />
+            </>
+          )}
         </Routes>
         <NavBar />
       </BrowserRouter>
