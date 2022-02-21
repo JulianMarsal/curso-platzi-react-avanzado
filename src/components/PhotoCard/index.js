f200e2a06a832d0485fa23da806bbb8796af3543
Tu nombre <tu@ejemplo.cojulian_1997_01@hotmail.com>
2022-02-21 17:21:37 -0300

    added functionality: like with registered user

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Article, ImgWrapper, Img } from "./styles";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNearScreen } from "../../hooks/useNearScreen";
 import { FavButton } from "../FavButton";
 import { useLikePhoto } from "../../hooks/useToggleLikeMutations";
@@ -9,15 +8,13 @@ import { Link } from "react-router-dom";
 const DEFAULT_IMAGE =
   "https://3.bp.blogspot.com/-T_2Mk0VWsPs/WKh_DNP_02I/AAAAAAAABF4/oBTlwNI52u8mdo9Y5deIxBzg7Em4n2pvQCLcB/s400/loading%2Bgif%2B1.gif";
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen();
-  const key = `like-${id}`;
-  const [liked, setLiked] = useLocalStorage(key, false);
+
   const [toggleLike] = useLikePhoto();
 
   const handleFavClick = () => {
-    setLiked(!liked);
-    !liked && toggleLike({ variables: { input: { id } } });
+    toggleLike({ variables: { input: { id } } });
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -2,12 +2,44 @@ import React from "react";
 import { useContext } from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { AppProvider } from "./context/AppContext";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  from,
+  HttpLink,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem("token");
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  return forward(operation);
+});
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === "invalid_token") {
+    window.sessionStorage.removeItem("token");
+    window.location = "/user";
+  }
+});
 
 const client = new ApolloClient({
-  uri: "https://petgram-backend-one.vercel.app/graphql",
   cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: "https://petgram-backend-one.vercel.app/graphql",
+    }),
+  ]),
 });
 
 ReactDOM.render(
