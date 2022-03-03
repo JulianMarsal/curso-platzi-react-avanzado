ccf6eba2b28ddab7eb97f673fd191266ff81d608
Tu nombre <tu@ejemplo.cojulian_1997_01@hotmail.com>
2022-03-03 02:01:32 -0300

    Page 404 added and redirections for non login user

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { Home } from "./pages/Home";
 import { NavBar } from "./components/NavBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Detail } from "./pages/Detail";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { Favs } from "./pages/Favs";
 import { useContext } from "react";
 import { AppContext } from "./context/AppContext";
+import { NotFound } from "./pages/NotFound";
 export const App = () => {
   const {
     user: { isAuth },
@@ -21,20 +22,26 @@ export const App = () => {
         <GlobalStyle />
         <Logo />
         <Routes>
-          <Route path="/detail/:detailId" element={<Detail />} />
-          <Route path="/pet/:id" element={<Home />} />
           <Route path="/" element={<Home />} />
-          {isAuth ? (
-            <>
-              <Route path="/favs" element={<Favs />} />
-              <Route path="/user" element={<User />} />
-            </>
-          ) : (
-            <>
-              <Route path="/favs" element={<Favs />} />
-              <Route path="/user" element={<NotRegisteredUser />} />
-            </>
-          )}
+          <Route path="/pet/:id" element={<Home />} />
+          <Route path="/detail/:detailId" element={<Detail />} />
+
+          <Route
+            path="/favs"
+            element={isAuth ? <Favs /> : <NotRegisteredUser />}
+          />
+          <Route
+            path="/user"
+            element={isAuth ? <User /> : <NotRegisteredUser />}
+          />
+          <Route
+            path="/login"
+            element={
+              !isAuth ? <NotRegisteredUser /> : <Navigate replace to="/" />
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <NavBar />
       </BrowserRouter>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NotFound = () => {
+  return <h1>404 La Página Buscada No Existe</h1>;
+};
